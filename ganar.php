32554d4d3c47ebf7a19d34bbeff8b72933ef85d6
jvboronatjosep <jvboronatdavid@gmail.com>
2024-12-01 16:12:34 +0100

    creacion de archivos ganar perder con su respectivo style

---
+++ ganar.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Has Ganado</title>
+    <link rel="stylesheet" href="style_ganar_perder.css">
+</head>
+<body>
+    <div class="page-container">
+        <h1>¡Enhorabuena!</h1>
+        <p class="page-message">¡Has ganado! :)</p>
+        <a href="index.php">
+            <button class="page-button">Jugar de nuevo</button>
+        </a>
+    </div>
+</body>
+</html>
